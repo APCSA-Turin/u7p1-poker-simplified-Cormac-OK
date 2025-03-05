7bef5efd65c83443a6779ef9db8792196ed41d28
Cormac-OK <cormaco@nycstudents.net>
2025-03-05 13:00:03 +0000

    All tests passing

--- src/main/java/com/example/project/Game.java
+++ src/main/java/com/example/project/Game.java
@@ -1,4 +1,5 @@
 package com.example.project;
+
 import java.util.ArrayList;
 
 
@@ -8,24 +9,143 @@ public class Game{
         play();
     }
 
+    /**
+     * Determines which player has the better hand
+     * @param p1 Player 1
+     * @param p2 Player 2
+     * @param p1Hand Player 1's best hand
+     * @param p2Hand Player 2's best hand
+     * @param communityCards The community cards
+     * @return A string stating which player won, or if there was a tie
+     */
     public static String determineWinner(Player p1, Player p2,String p1Hand, String p2Hand,ArrayList<Card> communityCards){
-        return "Error";
+
+        // If the hands aren't the same, the better hand wins
+        if(!p1Hand.equals(p2Hand)){
+            if(Utility.getHandRanking(p1Hand) > Utility.getHandRanking(p2Hand)){
+                return "Player 1 wins!";
+            }
+            else{
+                return "Player 2 wins!";
+            }
+        }
+        // If the hands are tied:
+        else{
+            // In pair based hands, the highest pair wins
+            if(p1Hand.equals("Pair") || p1Hand.equals("Two Pair")){
+                if(p1.highestSet(2) > p2.highestSet(2)){
+                    return "Player 1 wins!";
+                }
+                else if (p2.highestSet(2) > p1.highestSet(2)){
+                    return "Player 2 wins!";
+                }
+                else{
+                    return "Tie!";
+                }
+            }
+            // The highest three set wins
+            else if (p1Hand.equals("Three of a Kind")){
+                if(p1.highestSet(3) > p2.highestSet(3)){
+                    return "Player 1 wins!";
+                }
+                else if (p2.highestSet(3) > p1.highestSet(3)){
+                    return "Player 2 wins!";
+                }
+                else{
+                    return "Tie!";
+                }
+            }
+            // In a full house, the higher set of 3 wins, and if they're equal, the higher pair wins
+            else if (p1Hand.equals("Full House")){
+                // Checking set of three
+                if(p1.highestSet(3) > p2.highestSet(3)){
+                    return "Player 1 wins!";
+                }
+                else if (p2.highestSet(3) > p1.highestSet(3)){
+                    return "Player 2 wins!";
+                }
+                // Checking pair
+                else{
+                    ArrayList<Integer> p1Freq = p1.findRankingFrequency();
+                    ArrayList<Integer> p2Freq = p2.findRankingFrequency();
+
+                    // Finding the ranks of the pairs
+                    int p1PairRank = 0;
+                    for(int i = 0; i < p1Freq.size(); i ++){
+                        if(p1Freq.get(i) == 2){
+                            p1PairRank = i;
+                        }
+                    }
+                    
+                    int p2PairRank = 0;
+                    for(int i = 0; i < p2Freq.size(); i ++){
+                        if(p2Freq.get(i) == 2){
+                            p2PairRank = i;
+                        }
+                    }
+
+                    if(p1PairRank > p2PairRank){
+                        return "Player 1 wins!";
+                    }
+                    else if (p2PairRank > p1PairRank){
+                        return "Player 2 wins!";
+                    }
+                    else{
+                        return "Tie!";
+                    }
+                }
+            }
+            // The highest set of for wins
+            else if (p1Hand.equals("Four of a Kind")){
+                if(p1.highestSet(4) > p2.highestSet(4)){
+                    return "Player 1 wins!";
+                }
+                else if (p2.highestSet(4) > p1.highestSet(4)){
+                    return "Player 2 wins!";
+                }
+                else{
+                    return "Tie!";
+                }
+            }
+            // In all cases, the better hand will be the one with the highest individual card
+            else{
+                ArrayList<Card> p1Cards = p1.getHand();
+                ArrayList<Card> p2Cards = p2.getHand();
+
+                int p1MaxRank = 0;
+                int p2MaxRank = 0;
+
+                for(Card card : p1Cards){
+                    if(Utility.getRankValue(card.getRank()) > p1MaxRank){
+                        p1MaxRank = Utility.getRankValue(card.getRank());
+                    }
+                }
+
+                for(Card card : p2Cards){
+                    if(Utility.getRankValue(card.getRank()) > p2MaxRank){
+                        p2MaxRank = Utility.getRankValue(card.getRank());
+                    }
+                }
+
+                System.out.println(p1MaxRank);
+                System.out.println(p2MaxRank);
+
+                if(p1MaxRank > p2MaxRank){
+                    return "Player 1 wins!";
+                }
+                else if(p2MaxRank > p1MaxRank){
+                    return "Player 2 wins!";
+                }
+            }
+
+
+
+        }
+        return "Tie!";
     }
 
     public static void play(){ //simulate card playing
-        Player player = new Player();
-        player.addCard(new Card("7", "♠"));
-        player.addCard(new Card("7", "♦"));
-      
-        
-        // Community Cards
-        ArrayList<Card> communityCards = new ArrayList<>();
-        communityCards.add(new Card("8", "♣"));
-        communityCards.add(new Card("8", "♥"));
-        communityCards.add(new Card("A", "♠"));
-        
-        player.playHand(communityCards);
-        String handResult = player.playHand(communityCards);
+
     }
         
         
--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -19,7 +19,7 @@ public class Player{
         hand.add(c);
     }
 
-    public String playHand(ArrayList<Card> communityCards){      
+    public String playHand(ArrayList<Card> communityCards){
         allCards = new ArrayList<>();
         for(Card c : hand){
             allCards.add(c);
@@ -27,18 +27,81 @@ public class Player{
         for(Card c : communityCards){
             allCards.add(c);
         }
+        sortAllCards();
 
-        System.out.println(findRankingFrequency());
-        
-        return "Nothing";
+        String highHand = "Nothing"; // Default if no other hand is found
+
+        if(highCard(communityCards)){
+            highHand = "High Card";
+        }
+        if(countSets(2) == 1){
+            highHand = "A Pair";
+        }
+        if(countSets(2) == 2){
+            highHand = "Two Pair";
+        }
+        if(countSets(3) == 1){
+            highHand = "Three of a Kind";
+        }
+        if(straight()){
+            highHand = "Straight";
+        }
+        if(flush()){
+            highHand = "Flush";
+        }
+
+        /* A full house is one set of 3 of a kind, and a pair.
+         * I test for two pairs instead of one here because the set of 3 contains a pair.
+        */
+        if(countSets(3) == 1 && countSets(2) == 2){
+            highHand = "Full House";
+        }
+
+        if(countSets(4) == 1){
+            highHand = "Four of a Kind";
+        }
+
+        if(straight() && flush()){
+            highHand = "Straight Flush";
+        }
+
+        // A royal flush is a flush and a straight the lowest card of which is a 10
+        if(straight() && flush() && allCards.get(0).getRank().equals("10")){
+            highHand = "Royal Flush";
+        }
+                
+        return highHand;
     }
 
-    public void sortAllCards(){} 
+    /**
+     * Sorts the allCards ArrayList from cards of low rank to high rank
+     */
+    public void sortAllCards(){ // Uses selection sort
+        for(int i = 0; i < allCards.size()-1; i++){
+            Card min = allCards.get(i+1);
+            int minIndex = i+1;
+            for(int j = i + 1; j < allCards.size(); j++){
+                if (Utility.getRankValue(allCards.get(j).getRank()) < Utility.getRankValue(min.getRank())){
+                    min = allCards.get(j);
+                    minIndex = j;
+                }
 
+                if (Utility.getRankValue(min.getRank()) < Utility.getRankValue(allCards.get(i).getRank())){
+                    allCards.set(minIndex, allCards.get(i));
+                    allCards.set(i, min);
+                }
+            }
+        }
+    } 
+
+    /**
+     * Finds the frequency which each rank occurs in the player's hand
+     * @return A list of ints where the value at each index is the number of times a card of the corresponding rank appears in the player's complete hand
+     */
     public ArrayList<Integer> findRankingFrequency(){
         ArrayList<Integer> frequencies = new ArrayList<Integer>();
         for (int i = 0; i < 13; i++){
-            String rank = Utility.getRanks()[i];
+            String rank = ranks[i];
             frequencies.add(0);
             for(Card card : allCards){
                 if(card.getRank().equals(rank)){
@@ -49,10 +112,14 @@ public class Player{
         return frequencies; 
     }
 
+    /**
+     * Finds the frequency which each suit occurs in the player's hand
+     * @return A list of ints where the value at each index is the number of times a card of the corresponding suit appears in the player's complete hand
+     */
     public ArrayList<Integer> findSuitFrequency(){
         ArrayList<Integer> frequencies = new ArrayList<Integer>();
         for (int i = 0; i < 4; i++){
-            String suit = Utility.getSuits()[i];
+            String suit = suits[i];
             frequencies.add(0);
             for(Card card : allCards){
                 if(card.getSuit().equals(suit)){
@@ -63,13 +130,116 @@ public class Player{
         return frequencies; 
     }
 
-   
+    /**
+     * Tests if the player has a high card
+     * @param communityCards The community cards
+     * @return Whether the player has a high card
+     */
+    private boolean highCard(ArrayList<Card> communityCards){
+        Card max = hand.get(0);
+        boolean playerHasMax = true;
+        // Checks whether the player's first of second card is greater
+        if(Utility.getRankValue(hand.get(1).getRank()) > Utility.getRankValue(max.getRank())){
+            max = hand.get(1);
+        }
+
+        // Checks if the player's high card is greater than any of the community cards
+        for(Card card : communityCards){
+            if(Utility.getRankValue(card.getRank()) > Utility.getRankValue(max.getRank())){
+                playerHasMax = false;
+            }
+        }
+
+        return playerHasMax;
+        }
+
+    /**
+     * Counts the number of sets of (size) of a kind cards the player has
+     * @param size The size of sets to search for
+     * @return The number of sets found
+     */
+    private int countSets(int size){
+        int count = 0;
+        int last = -1;
+        int currentSize = 1;
+        
+        for(Card card : allCards){
+            if(Utility.getRankValue(card.getRank()) == last){ // Checks if current card matches previous
+                currentSize ++;
+            }
+            else{
+                currentSize = 1;
+            }
+            last = Utility.getRankValue(card.getRank());
+            if (currentSize == size){ // Checks if the current streak meets the size parameter
+                count ++;
+                currentSize = 1;
+                last = -1;
+            }
+        }
+        return count;
+    }
+
+    //TODO: Set highestSet back to private
+
+    /**
+     * Returns the rank of the highest set of size size
+     * @param size The size of the set to search for
+     * @return The rank of the highest ranking set
+     */
+    public int highestSet(int size){
+        int max = 0;
+        int last = -1;
+        int currentSize = 1;
+        
+        for(Card card : allCards){
+            if(Utility.getRankValue(card.getRank()) == last){ // Checks if current card matches previous
+                currentSize ++;
+            }
+            else{
+                currentSize = 1;
+            }
+            last = Utility.getRankValue(card.getRank());
+            if (currentSize == size){ // Checks if the current streak meets the size parameter
+                if(Utility.getRankValue(card.getRank()) > max){
+                    max = Utility.getRankValue(card.getRank());
+                }
+                currentSize = 1;
+                last = -1;
+            }
+        }
+        return max;
+    }
+
+    /**
+     * @return Whether the player's hand is a straight
+     */
+    private boolean straight(){
+        int last = Utility.getRankValue(allCards.get(0).getRank());
+        for (int i = 1; i < allCards.size(); i ++){ 
+            if(Utility.getRankValue(allCards.get(i).getRank()) != last + i){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @return Whether the player's hand is a flush
+     */
+    private boolean flush(){
+        ArrayList<Integer> freq = findSuitFrequency();
+        for (Integer num: freq){
+            if (num == 5){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
     @Override
     public String toString(){
         return hand.toString();
     }
-
-
-
-
 }
